ff509937fa554213cc6760cd7ffbd096983ce93b
user2 <user2@user2-PC>
2020-04-01 21:03:38 +0900

    #03.1 수정은?

--- git/src/com/git/controller/GitTestServlet.java
+++ git/src/com/git/controller/GitTestServlet.java
@@ -27,7 +27,7 @@ public class GitTestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("호호호 수정한 내용");
+		System.out.println("호호호ㅇㅇㅇ");
 	}
 
 	/**
