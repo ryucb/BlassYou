1408d38de54bb7e4d2034dfc8dbf62e924d24586
chillincoding <62998663+chillincoding@users.noreply.github.com>
2020-04-11 14:39:23 +0900

    #001 sebin project

---
+++ PETMILY_DEV/src/com/petmily/pet/controller/PetProfileDetailServlet.java
@@ -0,0 +1,43 @@
+package com.petmily.pet.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PetProfileDetailServlet
+ */
+@WebServlet("/user/petprofile/detail")
+public class PetProfileDetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PetProfileDetailServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int no = Integer.parseInt(request.getParameter("no"));
+		request.getRequestDispatcher("/views/user/petProfileDetail.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
